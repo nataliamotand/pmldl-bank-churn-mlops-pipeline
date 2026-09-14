c22476e06fd2d5fce2575415e7c8954234278588
Nat <nataliamotand@gmail.com>
2026-09-14 12:58:30 +0300

    Add model training script with MLflow tracking

---
+++ code/models/train_model.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, roc_auc_score
+import joblib
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+train_df = pd.read_csv("data/processed/train.csv")
+test_df = pd.read_csv("data/processed/test.csv")
+
+#one-hot encoding
+train_df = pd.get_dummies(train_df, columns=["Geography", "Gender"], drop_first=True)
+test_df = pd.get_dummies(test_df, columns=["Geography", "Gender"], drop_first=True)
+
+X_train = train_df.drop(columns=["Exited"])
+y_train = train_df["Exited"]
+
+X_test = test_df.drop(columns=["Exited"])
+y_test = test_df["Exited"]
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+with mlflow.start_run():
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train_scaled, y_train)
+
+    y_pred = model.predict(X_test_scaled)
+    y_pred_proba = model.predict_proba(X_test_scaled)[:, 1]
+
+    accuracy = accuracy_score(y_test, y_pred)
+    roc_auc = roc_auc_score(y_test, y_pred_proba)
+
+    mlflow.log_param("model_type", "LogisticRegression")
+    mlflow.log_param("max_iter", 1000)
+
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("roc_auc", roc_auc)
+
+    mlflow.sklearn.log_model(model, name="model")
+
+    print(f"Accuracy: {accuracy:.4f}")
+    print(f"ROC-AUC: {roc_auc:.4f}")
+    joblib.dump(model, "models/model.pkl")
